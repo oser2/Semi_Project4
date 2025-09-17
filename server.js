aee90a85462a0015d7cc3aaf823ebe931daca7eb
o_ser <ostapius.2009@gmail.com>
2025-09-17 19:46:11 +0300

    server_change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(router);
 server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running');
 });
-javascriptserver.use((req, res, next) => {
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
